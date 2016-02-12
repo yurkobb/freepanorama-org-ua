c16b865e3fb8c83897408c4bc549dfecb827458e
Yury Bulka <setthemfree@privacyrequired.com>
2016-02-12 14:57:00 +0200

    Додати віконце про фотофлешмоб (укр. версія тільки)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+jQuery(function ($) {
+    /* The photoflashmob button popup */
+    var flashmob = {
+        desc: $('#photoflashmob'),
+        button: $('.button.photoflashmob')
+    };
 
+    flashmob.desc.addClass('mfp-hide');
+
+    flashmob.button.magnificPopup({
+        type: 'inline'
+    });
+});
