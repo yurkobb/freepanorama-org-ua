eb4a7ff5c35123267bc0112454555eb17c74a119
Yury Bulka <setthemfree@privacyrequired.com>
2016-02-27 22:46:34 +0200

    Вилучити фотофлешмоб

--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 jQuery(function ($) {
-    /* The photoflashmob button popup */
-    var flashmob = {
-        desc: $('#photoflashmob'),
-        button: $('.button.photoflashmob')
-    };
-
-    flashmob.desc.addClass('mfp-hide');
-
-    flashmob.button.magnificPopup({
-        type: 'inline'
-    });
+    // Nothing here for now
+    return true;
 });
